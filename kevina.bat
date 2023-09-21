96b52a3109ca9b2388287069ca741656a26c250f
Kevin0918-rgb <84979598+Kevin0918-rgb@users.noreply.github.com>
2023-09-21 10:22:22 -0400

    CREAR ARCHIVOS Y DIRECTORIOS
    Este es un tipo de bat file que permite crear archivos y crear otros archivos de texto

---
+++ kevina.bat
@@ -0,0 +1,30 @@
+@echo off
+
+mkdir PRINCIP
+cd PRINCIP
+mkdir DOCS
+mkdir PROGRAM
+mkdir APUNTES
+cd DOCS
+mkdir WORD
+mkdir EXCEL
+cd ../APUNTES
+mkdir SISTEMAS
+cd SISTEMAS
+echo TEMA #1 > TEMA1.doc
+echo TEMA #2 > Tema2.doc
+cd ..
+cd ..
+cd DOCS
+cd WORD
+echo DOCUMENTO #1 > Doc1.txt
+cd ..
+cd EXCEL
+echo DOCUMENTO #2 > Doc2.txt
+
+cd ../../../
+
+cd PRINCIP
+echo Fichero #1 >>fichero1.txt
+echo Fichero #2>>fichero2.txt
+
