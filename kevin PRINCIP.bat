a5e0afeb85b04f99f374494f71e06d5a9b6dfe09
Kevin0918-rgb <84979598+Kevin0918-rgb@users.noreply.github.com>
2023-09-21 10:20:59 -0400

    CREATE FILES AND ADD THINGS

---
+++ kevin PRINCIP.bat
@@ -0,0 +1,38 @@
+@echo off
+
+mkdir PRINCIP
+cd PRINCIP
+mkdir DOCS | mkdir PROGRAM | mkdir APUNTES
+cd DOCS
+mkdir WORD | mkdir EXCEL
+cd WORD
+echo hola >>DOC1.txt
+cd ..
+cd EXCEL
+echo hola >>DOC2.txt
+cd ..
+cd ..
+cd PROGRAM
+mkdir apunntes
+cd apunntes
+mkdir SISTEMAS
+cd SISTEMAS
+echo hola >>Tema1.doc
+echo hola >>Tema2.doc
+cd ..
+cd ..
+cd ..
+cd PRINCIP
+cd APUNTES
+mkdir SISTEM 
+cd SISTEM
+echo hola >>Tema1.doc
+echo hola >>Tema2.doc
+cd ..
+cd ..
+echo Fichero #1 >>Fichero.txt
+echo Fichero #2 >>Fichero2.txt
+
+
+
+
